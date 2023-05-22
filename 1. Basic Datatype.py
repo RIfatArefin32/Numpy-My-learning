1325a8b244f8cf11b003d5c846d3ca6a4282a10c
Rifat Arefin <54114602+RIfatArefin32@users.noreply.github.com>
2023-05-22 09:54:05 +0600

    Basic and creating upload

---
+++ 1. Basic Datatype.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+#numpy.array(object, dtype = None, copy = True, order = None, subok = False, ndmin = 0)
+
+#introduction
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+arr = np.array([[1, 2], [3, 4]])
+print(arr)
+
+arr = np.array([1, 2, 3, 4], ndmin = 1)
+print(arr)
+
+arr = np.array([1, 2, 3], dtype=complex)
+print(arr)
+
+#Data types:
+'''
+bool_ (1B)
+int_ (4B or 8B) intc (4B or 8B): identical of int in C
+int8, int16, int32, int64
+uint8, uint16, uint32, uint64
+float16, float32, float64, float_ (shorthand for float64)
+complex64, complex128, complex_ (shorthand for complex128)
+'''
+arr = np.array([1, 2, 3], dtype=np.int32)
+print(arr.itemsize) #return the size of items in the array in Bytes
+
+#Structure Data Type
+dataType = np.dtype([('age',np.int64)]) 
+print(dataType)
+arr = np.array([(10), (20 ), (30 )], dtype=dataType)
+print(arr)
+
+'''
+'b'     boolean
+
+'i'     (signed) integer
+
+'u'     unsigned integer
+
+'f'     floating-point
+
+'c'     complex-floating point
+
+'m'     timedelta
+
+'M'     datetime
+
+'O'     (Python) objects
+
+'S','a' (byte-)string
+
+'U'     Unicode
+
+'V'      raw data (void)
+'''
+
+student = np.dtype([('name', 'S'), ('roll', np.int8), ('cgpa', np.float32)])
+print(student)
+arr = np.array([('Rifat', 117, 3.8), ('Mahimaa', 73, 3.5)])
+print(arr)
---
+++ 2. Array Creation.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+#Creating an empty array
+empArray = np.empty((3, 3), dtype=np.int8)
+print(empArray, "\n")
+
+
+
+#Creating array with values 0
+zeroArray = np.zeros((2), dtype=np.int8)
+print("1D array: \n", zeroArray, "\n")
+
+zeroArray = np.zeros((2, 2), dtype=np.int8)
+print("2D array: \n", zeroArray, "\n")
+
+zeroArray = np.zeros((2, 2, 2), dtype=np.float32)
+print("3D array: \n", zeroArray, "\n")
+
+
+
+#Creating array with value 1
+oneArray = np.ones((2), dtype=np.int8)
+print("1D array:\n", oneArray, "\n")
+
+oneArray = np.ones((2, 2), dtype=np.int8)
+print("2D array:\n", oneArray, "\n")
+
+
+
+#Creating array full with specified value:
+fullArray = np.full((2, 3), .52, dtype=np.float32)
+print(fullArray)
+
+
+
+#Creating array like another array
+arr = np.array([1, 2, 3], dtype=np.int8)
+print(arr, "\n")
+
+emptyLike = np.empty_like(arr)
+print(emptyLike, "\n")
+
+zerosLike = np.zeros_like(arr)
+print(zerosLike, "\n")
+
+onesLike = np.ones_like(arr)
+print(onesLike, "\n")
+
+
+arr = np.zeros((2, 2), dtype=np.int32)
+for i in range (arr.shape[0]):
+    for j in range(arr.shape[1]):
+        arr[i][j] = i+j
+print(arr, "\n")
+
+emptyLike = np.empty_like(arr)
+print(emptyLike, "\n")
+
+zerosLike = np.zeros_like(arr)
+print(zerosLike, "\n")
+
+onesLike = np.ones_like(arr)
+print(onesLike, "\n")
+
+fullArray = np.full_like(arr, 5555)
+print(fullArray, "\n")
+
+
+
+#Creating array with arange() function
+#Syntax: numpy.arange(start=0, stop, step=1, dtype)
+arr = np.arange(24)
+print(arr)
+
+arr = np.arange(20, 30, 2)
+print(arr)
+
+arr = np.arange(2, 20, 3, dtype=np.float_)
+print(arr)
+
+
+
+#Creating array using shape() and reshape()
+arr = np.arange(2, 13, 2)
+print("Arr is: ", arr, "\n")
+arr.shape = (2, 3)
+print("After shaping:\n", arr, '\n')
+
+newArr = arr.reshape(3, 2)
+print("newArray: \n", newArr, "\n")
+print("OldArray: \n", arr, "\n")
